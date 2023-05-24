2e3bfe9d332c9a75ed5c4e2794e456b8e7c07300
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-05-24 15:09:35 +0200

    created inputfield

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Inputfield from "../components/login/Inputfield";
 
 export default function LoginPage() {
+  const [userCredentail, setUserCredential] = useState(undefined);
+
+  console.log(userCredentail);
+
   return (
     <form>
       <h3>Login</h3>
+      <Inputfield setUserCredential={setUserCredential} passwordType="text" />
     </form>
   );
 }
