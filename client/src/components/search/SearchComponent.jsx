5780ad05a482e355972ab63e73b9712ad36d8ef7
ledei <holybuzz@live.se>
2023-05-31 03:25:51 +0200

    commented on searchComponent

--- client/src/components/search/SearchComponent.jsx
+++ client/src/components/search/SearchComponent.jsx
@@ -1,3 +1,8 @@
+/*
+seach component is a input field that send it value to a parent that uses useState
+the parent send setQuary from a useState that search component takes and send its value thru
+ */
+
 import React from "react";
 
 export default function SearchComponent({ setQuary }) {
