c7803757b5e4f82087a42a637299f40e58641910
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-06-01 23:47:30 +0200

    final CSS touches

--- client/src/components/search/SearchComponent.jsx
+++ client/src/components/search/SearchComponent.jsx
@@ -8,11 +8,17 @@ the parent send setQuary from a useState that search component takes and send it
  */
 
 import React from "react";
+import "./searchcomponent.css";
 
 export default function SearchComponent({ setQuary }) {
   return (
     <div className="search-container">
-      <input className="search" type="text" placeholder="Type to search" onChange={(e) => setQuary(e.target.value)} />
+      <input
+        className="search"
+        type="text"
+        placeholder="Type to search"
+        onChange={(e) => setQuary(e.target.value)}
+      />
     </div>
   );
 }
